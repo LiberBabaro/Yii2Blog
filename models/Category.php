82d32e7495b2f6e241a1b9f44c41f4bd19fd7426
Ilya Soldatkin <soldatkinilya1996@gmail.com>
2020-12-20 06:43:53 +0500

    generate models via GII

---
+++ models/Category.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "tbl_category".
+ *
+ * @property int $id
+ * @property string|null $title
+ */
+class Category extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'tbl_category';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+        ];
+    }
+}
